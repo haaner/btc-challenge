596f7cce45d1b7890bd427fbf003ca45ce0893a1
Heiko Vogel <heiko.vogel@birdworx.de>
2024-12-28 09:24:00 +0100

    [TASK] create simple secp hnp test data

--- test.py
+++ test.py
@@ -5,9 +5,14 @@ if __package__:
     sys.path.append(path.dirname(path.abspath(__file__)))
 
 from sys import stdin
-from lib.secp256k1 import randrange, secp
+from lib.secp256k1 import secp
 
-d = 2 # secret key
+d = 5 # secret key
 dG = secp.mult(d, secp.g) # public key
 
 print(d, dG)
+
+for k in range(1, 5):
+    z = k + 100
+    r,s  = secp.sign(z, d, k)
+    print(r, s, z)
