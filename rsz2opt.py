c66ee15a700cd43399bcd9622f3e0fc289e47340
Heiko Vogel <heiko.vogel@birdworx.de>
2025-04-14 07:32:00 +0200

    noncense

--- rsz2opt.py
+++ rsz2opt.py
@@ -4,7 +4,8 @@ if __package__:
     from os import sys, path
     sys.path.append(path.dirname(path.abspath(__file__)))
 
-from sys import stdin
+import argparse
+from sys import argv, stdin
 from lib.secp256k1 import inverseMod, secp
 
 def inv(s):
@@ -67,7 +68,21 @@ for j in range(n + 1):
     print(0, end = ' ')
 print(']')    
 
-upper = [ g-1 for j in range(0, n+1) ]
+# Apply the nonce assumptions
+parser = argparse.ArgumentParser() 
+
+key = '--max-nonce-bits'
+parser.add_argument(key) 
+args = parser.parse_args()
+mnb = args.max_nonce_bits
+
+if mnb == None:
+    nonce_max = g
+else:
+    nonce_max = pow(2, int(mnb))
+
+upper = [ nonce_max-1 for j in range(0, n) ]
+upper.append(g-1)
 
 print('[', *upper, sep=' ', end=" ]\n")
 print('[', *modulo, sep=' ', end=' ]')
